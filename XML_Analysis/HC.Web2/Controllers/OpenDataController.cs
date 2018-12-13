d687e55a43b8308338cb067068fbb2261f1d18f1
林昕駿 <kk854002@gmail.com>
2018-12-13 23:25:07 +0800

    完成OpeDataWeb

--- XML_Analysis/HC.Web2/Controllers/OpenDataController.cs
+++ XML_Analysis/HC.Web2/Controllers/OpenDataController.cs
@@ -1,10 +1,12 @@
-﻿using HC.DataBase;
+﻿
+using HC.DataBase;
 using HC.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace HC.Web2.Controllers
 {
@@ -13,7 +15,7 @@ namespace HC.Web2.Controllers
         public IActionResult Index()
         {
             OpenDataDbContext db = new OpenDataDbContext();
-
+           
             List<OpenData> models = db.OpenData.ToList();
 
             return View(models);
